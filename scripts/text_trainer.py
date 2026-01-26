83ae5f8a9afc1b5fde17733b90cf6ca3c528b624
iamnotpi <ductrung27.dt@gmail.com>
2026-01-26 10:38:09 +0700

    Minor

--- scripts/text_trainer.py
+++ scripts/text_trainer.py
@@ -92,7 +92,6 @@ def create_config(task_id, model, dataset, dataset_type, file_format, output_dir
     if log_wandb:
         config["wandb_runid"] = f"{task_id}_{expected_repo_name}"
         config["wandb_name"] = f"{task_id}_{expected_repo_name}"
-        config["wandb_mode"] = "offline"
         os.makedirs(train_cst.WANDB_LOGS_DIR, exist_ok=True)
     else:
         for key in list(config.keys()):
