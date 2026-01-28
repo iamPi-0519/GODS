745e68a5a4e6de1f088b1c5826d770a9d0d56de8
iamnotpi <ductrung27.dt@gmail.com>
2026-01-28 11:11:43 +0700

    update

--- examples/run_environment_task.sh
+++ examples/run_environment_task.sh
@@ -19,10 +19,27 @@ DOCKER_BUILDKIT=1
 
 CHECKPOINTS_DIR="$(pwd)/secure_checkpoints"
 OUTPUTS_DIR="$(pwd)/outputs"
+DEBUG_DIR="$(pwd)/alfworld_debug"
 mkdir -p "$CHECKPOINTS_DIR"
 chmod 777 "$CHECKPOINTS_DIR"
 mkdir -p "$OUTPUTS_DIR"
 chmod 777 "$OUTPUTS_DIR"
+mkdir -p "$DEBUG_DIR"
+chmod 777 "$DEBUG_DIR"
+
+docker network create -d bridge train-environment-network
+
+docker run -d \
+  --name environment-server-0 \
+  --network train-environment-network \
+  -p 10000:8000 \
+  affinefoundation/agentgym:alfworld
+
+docker run -d \
+  --name environment-server-1 \
+  --network train-environment-network \
+  -p 10001:8000 \
+  affinefoundation/agentgym:alfworld
 
 # Build the downloader image
 docker build -t trainer-downloader -f dockerfiles/trainer-downloader.dockerfile .
@@ -44,12 +61,21 @@ docker run --rm \
 
 
 docker run --rm --gpus all \
+  --network train-environment-network \
   --security-opt=no-new-privileges \
   --cap-drop=ALL \
   --memory=64g \
   --cpus=8 \
+  -e WANDB_API_KEY="$WANDB_TOKEN" \
+  -e WANDB_TOKEN="$WANDB_TOKEN" \
+  -e HUGGINGFACE_TOKEN="$HUGGINGFACE_TOKEN" \
+  -e HUGGINGFACE_USERNAME="$HUGGINGFACE_USERNAME" \
+  -e WANDB_MODE="online" \
+  -e ENVIRONMENT_SERVER_URLS="http://environment-server-0:8000,http://environment-server-1:8000" \
+  -e PYTORCH_CUDA_ALLOC_CONF="expandable_segments:True" \
   --volume "$CHECKPOINTS_DIR:/cache:rw" \
   --volume "$OUTPUTS_DIR:/app/checkpoints/:rw" \
+  --volume "$DEBUG_DIR:/workspace/axolotl/alfworld_debug:rw" \
   --name grpo-text-trainer-example \
   standalone-text-trainer \
   --task-id "$TASK_ID" \
